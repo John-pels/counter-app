20db8c6d59a068bd334ee99a9507a00e25d7a044
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-10-25 16:30:33 +0100

    I remove the local state so as to use a single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,12 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -29,13 +21,13 @@ class Counter extends Component {
     );
   }
   formatCount = () => {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   };
 
   getBadgeClasses() {
     let classes = "badge m-3 mt-3 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
